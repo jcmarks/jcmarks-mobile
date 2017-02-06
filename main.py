2355afd0dffcc43ce35154b2ca46f641352190cf
jcmarks <jcmarks22@gmail.com>
2017-02-05 21:35:01 -0500

    update main.py
    reverting to byte 1

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ import os
 import jinja2
 import webapp2
 import logging
-import json
-import urllib
-
-# this is used for constructing URLs to google's APIS
-from googleapiclient.discovery import build
-
-API_KEY = 'AIzaSyDrr-4elELZpZurOEVIK6hBsBf6WnSiySs'
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
