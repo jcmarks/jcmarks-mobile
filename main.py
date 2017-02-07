5946f059c22cf4f549a0e553f5b40c6ad486d36c
Jordan Marks <jcmarks22@gmail.com>
2017-02-06 21:22:38 -0500

    updating html files

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import logging
 import json
 import urllib
 
-# this is used for constructing URLs to google's APIS
-from googleapiclient.discovery import build
-
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
@@ -18,16 +15,28 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+# this is used for constructing URLs to google's APIS
+from googleapiclient.discovery import build
+
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 API_KEY = 'AIzaSyDhbRM4UUBRqouCvuTD-eNMTcdUx7ukN1k'
+TABLE_ID = '1UvKGchZrRW4BR-bQ8hGzNu2U22Ab3lazVB7Rf2w'
+
+# This uses discovery to create an object that can talk to the 
+# fusion tables API using the developer key
+service = build('fusiontables', 'v1', developerKey=API_KEY)
 
 @app.route('/')
 def hello():
     template = JINJA_ENVIRONMENT.get_template('templates/index.html')
     return template.render()
 
+@app.route('/about')
+def about():
+    template = JINJA_ENVIRONMENT.get_template('templates/about.html')
+    return template.render()
 
 @app.errorhandler(404)
 def page_not_found(e):
