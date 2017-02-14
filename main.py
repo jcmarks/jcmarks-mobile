25f07411b5c66cd52dffc0d5316bdbef591c2fae
Jordan Marks <jcmarks22@gmail.com>
2017-02-14 15:40:56 -0500

    following tutorial

--- main.py
+++ main.py
@@ -5,32 +5,40 @@ import webapp2
 import logging
 import json
 import urllib
+import MySQLdb
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+    
+_INSTANCE_NAME = 'jcmarks-mobile:mobile-data'
+_DB_NAME = 'mobile_data'
+_USER = 'root'
+_PSWD = 'staph-288'
+
+_ACTIVITY = 'plugin_google_activity_recognition'
+_LOCATIONS = 'locations'
+_EPSILON = 1
 
 from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-# this is used for constructing URLs to google's APIS
-from googleapiclient.discovery import build
-
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
-
-API_KEY = 'AIzaSyDhbRM4UUBRqouCvuTD-eNMTcdUx7ukN1k'
-TABLE_ID = '1UvKGchZrRW4BR-bQ8hGzNu2U22Ab3lazVB7Rf2w'
-
-# This uses discovery to create an object that can talk to the 
-# fusion tables API using the developer key
-service = build('fusiontables', 'v1', developerKey=API_KEY)
+if (os.getenv('SERVER_SOFTWARE') and
+    os.getenv('SERVER_SOFTWARE').startswith('Google App Engine/')):
+    _DB = MySQLdb.connect(unix_socket='/cloudsql/' + _INSTANCE_NAME, db=_DB_NAME, user=_USER, charset='utf8')
+else:
+    _DB = MySQLdb.connect(host='127.0.0.1', port=3306, db=_DB_NAME, user=_USER, passwd=_PSWD, charset='utf8')
 
 @app.route('/')
-def hello():
+def index():
     template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    cursor = _DB.cursor()
+    cursor.execute('SHOW TABLES')    
+    logging.info(cursor.fetchall())
+
+    
     return template.render()
 
 @app.route('/about')
@@ -38,6 +46,11 @@ def about():
     template = JINJA_ENVIRONMENT.get_template('templates/about.html')
     return template.render()
 
+@app.route('/quality')
+def about():
+    template = JINJA_ENVIRONMENT.get_template('templates/quality.html')
+    return template.render()
+    
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
