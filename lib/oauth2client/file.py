de92f86eaa3958a12d4bc489600d3382d1f3a7d8
Jordan Marks <jcmarks22@gmail.com>
2017-02-19 10:21:20 -0500

    jens base code

--- lib/oauth2client/file.py
+++ lib/oauth2client/file.py
@@ -21,16 +21,43 @@ credentials.
 import os
 import threading
 
-from oauth2client import _helpers
-from oauth2client import client
+from oauth2client.client import Credentials
+from oauth2client.client import Storage as BaseStorage
 
 
-class Storage(client.Storage):
+__author__ = 'jcgregorio@google.com (Joe Gregorio)'
+
+
+class CredentialsFileSymbolicLinkError(Exception):
+    """Credentials files must not be symbolic links."""
+
+
+class Storage(BaseStorage):
     """Store and retrieve a single credential to and from a file."""
 
     def __init__(self, filename):
-        super(Storage, self).__init__(lock=threading.Lock())
         self._filename = filename
+        self._lock = threading.Lock()
+
+    def _validate_file(self):
+        if os.path.islink(self._filename):
+            raise CredentialsFileSymbolicLinkError(
+                'File: %s is a symbolic link.' % self._filename)
+
+    def acquire_lock(self):
+        """Acquires any lock necessary to access this Storage.
+
+        This lock is not reentrant.
+        """
+        self._lock.acquire()
+
+    def release_lock(self):
+        """Release the Storage lock.
+
+        Trying to release a lock that isn't held will result in a
+        RuntimeError.
+        """
+        self._lock.release()
 
     def locked_get(self):
         """Retrieve Credential from file.
@@ -39,10 +66,10 @@ class Storage(client.Storage):
             oauth2client.client.Credentials
 
         Raises:
-            IOError if the file is a symbolic link.
+            CredentialsFileSymbolicLinkError if the file is a symbolic link.
         """
         credentials = None
-        _helpers.validate_file(self._filename)
+        self._validate_file()
         try:
             f = open(self._filename, 'rb')
             content = f.read()
@@ -51,7 +78,7 @@ class Storage(client.Storage):
             return credentials
 
         try:
-            credentials = client.Credentials.new_from_json(content)
+            credentials = Credentials.new_from_json(content)
             credentials.set_store(self)
         except ValueError:
             pass
@@ -78,10 +105,10 @@ class Storage(client.Storage):
             credentials: Credentials, the credentials to store.
 
         Raises:
-            IOError if the file is a symbolic link.
+            CredentialsFileSymbolicLinkError if the file is a symbolic link.
         """
         self._create_file_if_needed()
-        _helpers.validate_file(self._filename)
+        self._validate_file()
         f = open(self._filename, 'w')
         f.write(credentials.to_json())
         f.close()
