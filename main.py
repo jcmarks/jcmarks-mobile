56240f2b0be6ac4739c21574d91f9ce7fed5d5c4
Jordan Marks <jcmarks22@gmail.com>
2017-02-20 11:09:32 -0500

    full mankoff template

--- main.py
+++ main.py
@@ -23,10 +23,10 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 # Define your production Cloud SQL instance information.
-_INSTANCE_NAME = 'your-project-id:your-instance-name'
-_DB_NAME = 'your-db-name'
+_INSTANCE_NAME = 'jcmarks-mobile:us-central1:mobile-data'
+_DB_NAME = 'mobile_data'
 _USER = 'root' # or whatever other user account you created
-_PSWD = 'your-password'
+_PSWD = 'staph-288'
 
 # the table where activities are logged
 _ACTIVITY = 'plugin_google_activity_recognition'
